8c459d7729e8dd87a6e3db22e91f7c83bfbf818e
hgbgrq <57474913+hgbgrq@users.noreply.github.com>
2023-02-16 13:48:08 +0900

    init

---
+++ src/test/java/com/example/gss/GssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
